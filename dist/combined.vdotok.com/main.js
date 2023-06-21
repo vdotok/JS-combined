bba7f6f6f8d1bea481ab53a394c4206fa583cf29
asadmahmood <asad.mahmood@norgic.com>
2023-06-21 19:02:49 +0500

    new build

--- dist/combined.vdotok.com/main.js
+++ dist/combined.vdotok.com/main.js
@@ -5000,7 +5000,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
   production: false,
-  apiBaseUrl: 'https://q-tenant.vdotok.dev/API/v0/'
+  apiBaseUrl: 'https://tenant.vdotok.com/API/v0/'
 };
 /*
  * For easier debugging in development mode, you can import the following file
